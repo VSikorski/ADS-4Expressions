76e063599f4bfc27aa3726541112df3e1746894a
VSikorski <79637308+VSikorski@users.noreply.github.com>
2024-07-08 13:38:48 +0200

    Create mainInfix.c

---
+++ mainInfix.c
@@ -0,0 +1,9 @@
+/* mainPref.c for lab assignment 4 expressions, updated 2021 */
+
+#include "scanner.h"
+#include "infixExp.h"
+
+int main(int argc, char *argv[]) {
+  infixExpTrees();
+  return 0;
+}
